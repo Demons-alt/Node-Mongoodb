d10181f0f4bd9d53c8eb82f859cda21204c2a590
Demons-alt <fahm3411@gmail.com>
2022-10-11 09:32:33 +0700

    menambahkan

--- module/mongoos.js
+++ module/mongoos.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
-const uri = "mongodb+srv://Mie:j5pP5LwHNPVxi995@cluster.xajie2t.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Mie:<password>@cluster.xajie2t.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect( uri ,{ 
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     // useCreateIndex: true,
 })
 
+mongoose.connection
+.once("open", ()=>console.log("Connected"))
+.on("error", error =>{
+    console.log("error", error)
+})
 
 module.exports = mongoose
